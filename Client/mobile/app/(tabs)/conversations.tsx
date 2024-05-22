fccf120a9aab6a84a5625f43e3b64cc697feaf2b
Nam Pham <pvnam95@hotmail.com>
2024-05-22 23:30:37 +0900

    feat: Add useState hook and conversations state to ConversationsScreen

--- Client/mobile/app/(tabs)/conversations.tsx
+++ Client/mobile/app/(tabs)/conversations.tsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import { View, Text } from "react-native";
+interface User { 
+
+}
+interface Conversation {
+    id: number;
+    users: User[];
+}
 
 export default function ConversationsScreen() { 
+    const [conversations, setConversations] = useState<Conversation[]>([])
     return (
         <View>
             <Text>ConversationsScreen</Text>
