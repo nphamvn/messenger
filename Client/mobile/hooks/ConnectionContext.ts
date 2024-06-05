96cab06d08003e05c6da3eff6332d51c5c4574a5
Nam Pham <nampv@fieldwork-g.com>
2024-06-05 16:24:09 +0900

    chore: Remove unused ConnectionContext import in mobile app

--- Client/mobile/hooks/ConnectionContext.ts
+++
@@ -1,6 +0,0 @@
-import * as signalR from "@microsoft/signalr";
-import { createContext } from "react";
-
-const ConnectionContext = createContext<signalR.HubConnection | null>(null);
-
-export default ConnectionContext;
