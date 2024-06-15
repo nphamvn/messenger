cc9b21386cc08d44548b6745d9a055432b7d0807
Nam Pham <pvnam95@hotmail.com>
2024-06-16 00:40:01 +0900

    add Required

--- Server/Api/Models/Messages/PostMessagePayload.cs
+++ Server/Api/Models/Messages/PostMessagePayload.cs
@@ -1,9 +1,12 @@
-﻿namespace Api.Models.Messages;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Models.Messages;
 
 public class PostMessagePayload
 {
     public int? ConversationId { get; set; }
     public List<string> MemberIds { get; set; } = [];
+    [Required]
     public string Text { get; set; }
     public string? ClientMessageId { get; set; }
 }
